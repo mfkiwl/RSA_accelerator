eb23a6595dd9c4907e168d4155d532082cfe5023
Emily Pakulski <enp2111@columbia.edu>
2015-04-01 00:06:35 -0400

    Manually merged Jaykar's changes. PLEASE START USING GIT JAYKAR

--- hello.c
+++ hello.c
@@ -13,92 +13,103 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "rsa_box.h"
 
-#define X_VAL 	0
-#define Y_VAL 	1
-#define X_MAX 	640
-#define Y_MAX 	480
-#define RADIUS 	40
-#define SPEED 	6000
+#define BIT_WIDTH 	128
+#define INT_SIZE	32
+#define RSA_SIZE 	BIT_WIDTH / INT_SIZE 
+static int BIT_SEGMENTS[RSA_SIZE] =  {0, 2, 4, 6}; 
 
-int rsab_fd;
+int vga_led_fd;
 
-/* Read and print the segment values */
-void print_segment_info() 
+//function to write a 128 bit int, sent to kernel space as an int array of size 4
+void write_segment(unsigned int *bit_input)
 {
-  rsa_box_arg_t vla;
-  int i;
+    rsa_box_arg_t rsa_userspace_vals;
+    int i; 
+    for(i = 0; i < RSA_SIZE; i++)
+    {
+    	rsa_userspace_vals.digit =  BIT_SEGMENTS[i];
+    	rsa_userspace_vals.segments =  bit_input[i];
 
-  for (i = 0 ; i < REGISTERS; i++) {
-      vla.address = i;
-      if (ioctl(rsab_fd, RSA_BOX_READ_DIGIT, &vla)) {
-           perror("ioctl(RSA_BOX_READ_DIGIT) failed");
-           return;
-       }
-       printf("Decimal: %d\n", vla.coord);
+    	if (ioctl(vga_led_fd, RSA_BOX_WRITE_DIGIT, &rsa_userspace_vals))
+        {
+      	    perror("ioctl(RSA_BOX_WRITE_DIGIT) failed");
+    	}
     }
 }
 
-void write_segment(int axis, int coord)
+//function to read 128 bit from kernel space and store the value in bit_output in userspace
+void read_segment(unsigned int *bit_output)
 {
-    rsa_box_arg_t vla;
+    rsa_box_arg_t rsa_userspace_vals;
 
-    vla.address = axis;
-    vla.coord = coord;
-    
-    if (ioctl(rsab_fd, RSA_BOX_WRITE_DIGIT, &vla)) {
-      perror("ioctl(RSA_BOX_WRITE_DIGIT) failed");
-      return;
+    int i;    
+    for(i = 0; i < RSA_SIZE; i++){
+    	rsa_userspace_vals.digit = BIT_SEGMENTS[i];
+    	
+	// reads value from kernel space into user space
+	if (ioctl(vga_led_fd, RSA_BOX_READ_DIGIT, &rsa_userspace_vals)) {
+      		perror("ioctl(RSA_BOX_WRITE_DIGIT) failed");
+    	}
+        
+    	bit_output[i] =  rsa_userspace_vals.segments; 
     }
+}
 
+//function to print the 128 bit number, it just prints the sections rather than the value. 
+void print_128_bit_integer(unsigned int *input_x){
+   int i;  
+   
+   for(i = 0; i < RSA_SIZE; i++)
+   {
+	printf("Quartile(%d): %u\n", i+1, input_x[i]);  
+   }
+}
+
+//function to test that the value sent to be written is equal to value read
+void check_equality(unsigned int *input_x, unsigned int *return_x){
+    int i; 
+    print_128_bit_integer(input_x);
+    print_128_bit_integer(return_x); 
+
+    for(i = 0; i < RSA_SIZE; i++)
+    {
+	if(!(input_x[i] == return_x[i]) )
+	{
+	    perror("Error reading/writing, values do not match\n"); 
+	}
+    }
+    printf("Read/Write Successful\n"); 
 }
 
 int main()
 {
-    rsa_box_arg_t vla;
-    static const char filename[] = "/dev/rsa_box"; // must match module name in rsa_box.c
+    int i;
+    int randx; 
+    unsigned int input_x[RSA_SIZE]; 
+    unsigned int return_x[RSA_SIZE]; 
+
+    static const char filename[] = "/dev/rsa_box";
 
     printf("RSA Box device driver started\n");
 
-    if ( (rsab_fd = open(filename, O_RDWR)) == -1) {
+    if ( (vga_led_fd = open(filename, O_RDWR)) == -1) {
         fprintf(stderr, "could not open %s\n", filename);
         return -1;
-    }
-  
-    // initalize location of ball
-    int coords[2] = {RADIUS + 1, RADIUS + 10};
-    write_segment(X_VAL, coords[X_VAL]);
-    write_segment(Y_VAL, coords[Y_VAL]); 
-    usleep(SPEED);
+    } 
  
-    int x_dir = 1;
-    int y_dir = 1;
-    
-    for(;;)
+    for(i=0; i < RSA_SIZE; i++)
     {
-        // change x-value if out of screen
-        if (coords[X_VAL] >=  X_MAX - RADIUS ||
-		coords[X_VAL] <= RADIUS)
-            x_dir *= -1; 
-        coords[X_VAL] += x_dir; 
-    
-        write_segment(X_VAL, coords[X_VAL]);
-
-        usleep(SPEED);
-
-        // change y-value if out of screen
-        if (coords[Y_VAL] >= Y_MAX - RADIUS || 
-		coords[Y_VAL] <= RADIUS)
-	    y_dir *= -1;
-        coords[Y_VAL] += y_dir;
-
-        write_segment(Y_VAL, coords[Y_VAL]);
-
-        usleep(SPEED);
+        input_x[i] = i; 
     }
+
+    write_segment(input_x);
+    read_segment(return_x);
+
+    check_equality(input_x, return_x);   
  
     printf("RSA Box device driver terminating\n");
-  
     return 0;
 }
