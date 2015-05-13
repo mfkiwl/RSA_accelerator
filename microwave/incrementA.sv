916f3f1ad65efa0c6f86920c5bb3a5fbc85c1628
jakenayeck <jan2150@columbia.edu>
2015-05-12 20:35:52 -0400

    increment bit position of a

--- microwave/incrementA.sv
+++ microwave/incrementA.sv
@@ -1,3 +1,18 @@
 /* verilator lint_off UNUSED */
-module incrementA(input logic reset, input logic clk);
+module incrementA(input logic reset, input logic clk, input logic[19:0] a, output logic out);
+
+        logic[4:0] counter; 
+
+        always_ff @(posedge clk)
+        begin
+            if(reset)
+            begin
+                counter<= 5'd0; 
+            end
+            else
+            begin
+                out <= a[counter]; 
+                counter <= counter + 1'b1; 
+            end
+        end
 endmodule
