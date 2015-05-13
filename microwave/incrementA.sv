90924145500f64ac8a110a79c92b20dc0c9da02c
jakenayeck <jan2150@columbia.edu>
2015-05-12 20:45:18 -0400

    add checking condition to 129

--- microwave/incrementA.sv
+++ microwave/incrementA.sv
@@ -1,18 +1,21 @@
 /* verilator lint_off UNUSED */
+/* verilator lint_off WIDTH */
 module incrementA(input logic reset, input logic clk, input logic[19:0] a, output logic out);
 
-        logic[4:0] counter; 
+logic[8:0] counter; 
 
-        always_ff @(posedge clk)
-        begin
-            if(reset)
-            begin
-                counter<= 5'd0; 
-            end
-            else
-            begin
-                out <= a[counter]; 
-                counter <= counter + 1'b1; 
-            end
-        end
+always_ff @(posedge clk)
+begin
+    if(reset)
+    begin
+        counter<= 8'd0; 
+    end
+    else
+    begin
+        out <= a[counter]; 
+        if(counter < 8'd129) begin
+            counter <= counter + 1'b1;
+        end 
+    end
+end
 endmodule
