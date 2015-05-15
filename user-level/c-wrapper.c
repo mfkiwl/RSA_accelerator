ac16897be115eee010b5a3973f0181b34698a362
Emily Pakulski <enp2111@columbia.edu>
2015-05-14 22:10:02 -0400

    added key_swap() function.

--- user-level/c-wrapper.c
+++ user-level/c-wrapper.c
@@ -18,9 +18,11 @@
 #include "../rsa_box.h"
 #include "instructions.h"
 #include "c-wrapper.h"
+#include "exteuc.h"
 
 void read_segment(int32_t *bit_output, int size);
 void send_bits(int32_t *value, int count); 
+
 
 /* globals */
 static int BIT_SEGMENTS[5] =  {1, 2, 3, 4, 5}; 
@@ -91,6 +93,36 @@ void send_bits(int32_t *value, int count)
 }
 
 /*
+ * Store private keys and get back our public key. 
+ */
+void key_swap(int32_t *p, int32_t *q, int32_t *our_n)
+{
+    int32_t p_phi[2];
+    int32_t q_phi[2];
+    int32_t phi_n[4];
+    int32_t d[4];
+
+    // calculate p - 1, q - 1
+    p_phi[0] = p[0] - 1;
+    p_phi[1] = p[1];
+
+    q_phi[0] = q[0] - 1;
+    q_phi[1] = q[1];
+
+    // store d, the extended euclid of (p - 1)(q - 1) and e
+    store_keys(PRIVATE, p_phi, q_phi);
+    read_our_N(phi_n);
+
+    int32_t E = 65537;
+    e_euclid(E, phi_n, d);
+    store_d(d);
+
+    // store actual p and q
+    store_keys(PRIVATE, p, q);
+    read_our_N(our_n);
+}
+
+/*
  * Stores keys into the specified registers, PUBLIC or PRIVATE
  * key registers.
  */
@@ -108,25 +140,22 @@ void store_keys(int type, int32_t *key_1, int32_t *key_2)
     {
         send_instruction(STORE_PUBLIC_KEY_1);
         send_bits(key_1, 4); // n 
-       
-        
-
 	send_instruction(STORE_PUBLIC_KEY_2);
         send_bits(key_2, 1); // e
     }
 }
 
 
-void store_d (int32_t *d){
-
-	send_instruction(STORE_D); 
-        send_bits(d, 4);
+void store_d(int32_t *d) 
+{
+    send_instruction(STORE_D); 
+    send_bits(d, 4);
 }
 
-void send_message2(int32_t *m){
-	
-	send_instruction(STORE_MESSAGE2); 
-	send_bits(m, 4); 
+void send_message2(int32_t *m)
+{	
+    send_instruction(STORE_MESSAGE2); 
+    send_bits(m, 4); 
 }
 
 /*
@@ -165,7 +194,6 @@ void read_our_public_key(int32_t *our_key)
  *
  * (Note: the interface to read private keys was intentionally ommitted.
  */
-
 void __read_encryption(int32_t *encryption)
 {
 	int32_t valid[5] = {0,0,0,0,0};
@@ -261,3 +289,114 @@ void read_output(int32_t *bit_output)
 {
     read_segment(bit_output, 4);
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/wait.h>
+
+#define READ_BUF 4096
+
+struct IntSet {
+	int x[4];
+};
+
+void err_sys(char *err) {
+	perror(err);
+	exit(1);
+}
+
+void e_euclid(int32_t e, int32_t phi[4], int32_t *d) 
+{
+	int phi1 = phi[3];
+	int phi2 = phi[2];
+	int phi3 = phi[1];
+	int phi4 = phi[0];
+
+	pid_t pid;
+	int fd[2];
+
+	if(pipe(fd) < 0) {
+		err_sys("pipe error");
+	}
+
+	if((pid = fork()) < 0) {
+		err_sys("fork error");
+	} 
+	else if(pid > 0) { // parent
+		close(fd[1]); // close write end
+
+		if(fd[0] != STDIN_FILENO) { // set STDIN
+			if(dup2(fd[0], STDIN_FILENO) != STDIN_FILENO) {
+				err_sys("dup2 error");
+			}
+		}
+
+		char buf[READ_BUF];
+		if(read(STDIN_FILENO, buf, READ_BUF) < 0) {
+			err_sys("read error");
+		}
+
+		// printf("[received]: %s\n", buf);
+
+		struct IntSet my_s;
+
+		/* parse buf */
+		const char s[2] = " ";
+		char *token = strtok(buf, s);
+		int curr = 0;
+
+		while(token != NULL && curr < 4) {
+			my_s.x[curr] = atoi(token);
+			printf("curr: %d, token: %s\n", curr, token);
+			token = strtok(NULL, s);
+			curr++;
+		}
+
+		if (waitpid(pid, NULL, 0) < 0)
+            err_sys("waitpid error");
+	
+        d[0] = my_s.x[3]; 
+        d[1] = my_s.x[2];
+        d[2] = my_s.x[1];
+        d[3] = my_s.x[0];
+} 
+	else { // child
+		close(fd[0]); // close read end
+
+		if(fd[1] != STDOUT_FILENO) { // set STDOUT
+			if(dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO) {
+				err_sys("dup2 error");
+			}
+		}
+
+		char e_s[READ_BUF]; 
+
+		char phi_s[READ_BUF];
+		char phi2_s[READ_BUF]; 
+		char phi3_s[READ_BUF];
+		char phi4_s[READ_BUF];
+		
+		snprintf(e_s, READ_BUF, "%d\n", e);
+
+		snprintf(phi_s, READ_BUF, "%d\n", phi1);
+		snprintf(phi2_s, READ_BUF, "%d\n", phi2);
+		snprintf(phi3_s, READ_BUF, "%d\n", phi3);
+		snprintf(phi4_s, READ_BUF, "%d\n", phi4);
+
+		printf("%s\n", e_s);
+
+		printf("%s\n", phi_s);
+		printf("%s\n", phi2_s);
+		printf("%s\n", phi3_s);
+		printf("%s\n", phi4_s);
+
+		// execute Python script
+		execlp("python", "python", "exteuc.py", e_s, phi_s, phi2_s, phi3_s, phi4_s, (char *)NULL);
+	}
+}
+
--- user-level/exteuc.h
+++ user-level/exteuc.h
@@ -4,6 +4,6 @@
 #include <stdint.h>     /* for unit32_t */
 
 void err_sys(char *err);
-void e_euclid(int e, int phi[4]);
+void e_euclid(int32_t e, int32_t phi[4], int32_t *d);
 
 #endif
